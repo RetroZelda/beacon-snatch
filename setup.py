ba5b7ceb3edfa332e5c5040b660969715b261964
retrozelda <efolckemer@retrozelda.com>
2025-10-05 02:51:38 -0400

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def parse_requirements(filename):
     
 setup(
     name="beacon-snatch",
-    version="0.1.7",
+    version="0.1.8",
     author="RetroZelda",
     author_email="retrozelda@gmail.com",
     description="Snatch from Beacon",
